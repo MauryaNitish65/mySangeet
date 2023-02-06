1bf747c06de743fa73e76e1e51a92b26f340aee7
nitishkumarmaurya <nitishkumarmaurya820.hitit2020@gmail.com>
2023-02-06 12:41:14 +0530

    Splash Screen Add

---
+++ app/src/main/java/com/example/mysangeet/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.mysangeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Intent iHome=new Intent(SplashScreenActivity.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(iHome);
+                finish();
+            }
+        },3000);
+    }
+}
